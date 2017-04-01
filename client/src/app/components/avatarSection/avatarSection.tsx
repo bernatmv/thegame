c051ab6aa2907ff008a10369e747f4a70f2720df
Bernat <bernatmv@gmail.com>
2017-04-01 13:20:05 +0200

    feat: styling the profile

--- client/src/app/components/avatarSection/avatarSection.tsx
+++ client/src/app/components/avatarSection/avatarSection.tsx
@@ -1,28 +1,32 @@
 import * as React from 'react';
 import { Image, Progress } from 'semantic-ui-react';
+import * as classnames from 'classnames';
 import AvatarSectionProps from './avatarSectionProps';
-import * as avatarImage from '../../../../assets/goblin_512x512_desaturated.png';
+//import * as avatarImage from '../../../../assets/goblin_512x512_desaturated.png';
 import * as style from './avatarSection.css';
 
 export default class AvatarSection extends React.Component<AvatarSectionProps, {}> {
     render(): JSX.Element {
         return (
-            <div className={style.container__game__avatar}>
-                <div className={style.container__game__avatar__profile}>
-                    <Image src={avatarImage} className={style.container__game__avatar__profile__image} />
-                    <div className={style.container__game__avatar__profile__status}>
-                        <div>┌<b>Morgobor</b>┘</div>
-                        <div>Hacker ─ Lvl 1</div>
-                        <div className={style.container__game__avatar__profile__status__row}>
-                            <div className={style.status__row__title}>HP</div> 
+            <div className={style.avatar}>
+                <div className={style.avatar__profile}>
+                    <div className={style.avatar__profile}>
+                        <div className={classnames(style.profile__row, style.profile__row_title)}>
+                            『<b>Morgobor</b>』
+                        </div>
+                        <div className={classnames(style.profile__row, style.profile__row_level)}>
+                            「Hacker ― LV1」
+                        </div>
+                        <div className={style.profile__row}>
+                            <div className={style.status__row__label}>HP</div> 
                             <Progress className={style.status__row__progress} color={'red'} value={100} total={100} progress={'ratio'} inverted={'true'} />
                         </div>
-                        <div className={style.container__game__avatar__profile__status__row}>
-                            <div className={style.status__row__title}>MP</div> 
+                        <div className={style.profile__row}>
+                            <div className={style.status__row__label}>MP</div> 
                             <Progress className={style.status__row__progress} color={'blue'} value={70} total={100} progress={'ratio'} inverted={'true'} />
                         </div>
-                        <div className={style.container__game__avatar__profile__status__row}>
-                            <div className={style.status__row__title}>SP</div> 
+                        <div className={style.profile__row}>
+                            <div className={style.status__row__label}>SP</div> 
                             <Progress className={style.status__row__progress} color={'yellow'} value={30} total={100} progress={'ratio'} inverted={'true'} />
                         </div>
                     </div>
