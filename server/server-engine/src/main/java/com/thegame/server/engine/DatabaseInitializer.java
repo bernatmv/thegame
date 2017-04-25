d20cc3aad13db3e3b0eb5a15e36a82acb2fb86f3
afarre <afarre@CROM>
2017-04-25 22:42:41 +0200

    fix:load initialization local folder dependent

--- server/server-engine/src/main/java/com/thegame/server/engine/DatabaseInitializer.java
+++ server/server-engine/src/main/java/com/thegame/server/engine/DatabaseInitializer.java
@@ -4,19 +4,20 @@ import com.owlike.genson.Genson;
 import com.owlike.genson.GensonBuilder;
 import com.thegame.server.common.functional.LambdaUtils;
 import com.thegame.server.common.logging.LogStream;
+import com.thegame.server.common.reflection.PackageUtils;
 import com.thegame.server.engine.intern.configuration.Configuration;
 import com.thegame.server.engine.messages.output.AreaMessageBean;
 import com.thegame.server.persistence.LocationDao;
 import com.thegame.server.persistence.PersistenceServiceFactory;
 import com.thegame.server.persistence.entities.Area;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 import com.thegame.server.engine.intern.services.MapperService;
 import com.thegame.server.persistence.entities.AreaExit;
 import com.thegame.server.persistence.entities.AreaExitId;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author afarre
@@ -25,34 +26,40 @@ public class DatabaseInitializer {
 	
 	private static final LogStream logger=LogStream.getLogger(DatabaseInitializer.class);
 	
-	private final Path assetsFolder;
+	private final String assetsPackage;
 	private final Genson genson;
 	private final LocationDao locationDao;
 	
 	
-	protected DatabaseInitializer(final Path _assetsFolder){
-		this.assetsFolder=_assetsFolder;
+	protected DatabaseInitializer(final String _assetsPackage){
+		this.assetsPackage=_assetsPackage;
 		this.genson=new GensonBuilder().useRuntimeType(true).create();
 		this.locationDao=PersistenceServiceFactory.LOCATIONDAO.getInstance(LocationDao.class);
 		logger.info("Current path: {}",Paths.get(".").toAbsolutePath().toString());
 	}
 	
 	public static DatabaseInitializer getInstance(){
-		return new DatabaseInitializer(Configuration.DATABASE_ASSETS_FOLDER.getPathValue());
+		return new DatabaseInitializer(Configuration.DATABASE_ASSETS_PACKAGE.getValue());
 	}
 	
+	protected Stream<String> getPackageResources(final String _package) throws IOException{
+		final List<String> explorePackages=Stream.of(_package)
+													.collect(Collectors.toList());
+		final List<String> resourcesFilter=Stream.of("*.json")
+													.collect(Collectors.toList());
+		return PackageUtils.getExistentResources(explorePackages,resourcesFilter)
+													.stream();
+	} 
 	
 	protected Stream<AreaMessageBean> loadAreas() throws IOException{
 		
-		return Files.list(this.assetsFolder.resolve("rooms"))
-			.peek(filePath -> logger.finest("database-initializer::initialize::{}::found::{}",Area.class.getSimpleName(),filePath))
-			.filter(filePath -> Files.isRegularFile(filePath))
-			.peek(filePath -> logger.finest("database-initializer::initialize::{}::found::{}::regular-file",Area.class.getSimpleName(),filePath))
-			.filter(filePath -> filePath.toString().endsWith(".json"))
-			.peek(filePath -> logger.finest("database-initializer::initialize::{}::found::{}::json",Area.class.getSimpleName(),filePath))
-			.filter(filePath -> Files.isReadable(filePath))
-			.peek(filePath -> logger.finest("database-initializer::initialize::{}::found::{}::readable",Area.class.getSimpleName(),filePath))
-			.map(filePath -> LambdaUtils.readAllBytes(filePath))
+		return getPackageResources(this.assetsPackage+".rooms")
+			.peek(resource -> logger.finest("database-initializer::initialize::{}::found::{}",Area.class.getSimpleName(),resource))
+			.filter(resource -> resource.endsWith(".json"))
+			.peek(resource -> logger.finest("database-initializer::initialize::{}::found::{}::json",Area.class.getSimpleName(),resource))
+			.map(resource -> resource.substring(0, resource.length()-5).replaceAll("\\.","/")+".json")
+			.peek(resource -> logger.finest("database-initializer::initialize::{}::found::{}::json",Area.class.getSimpleName(),resource))
+			.map(resource -> LambdaUtils.readAllBytesFromResource(resource))
 			.filter(optionalBytes -> optionalBytes.isPresent())
 			.map(optionalBytes -> optionalBytes.get())
 			.peek(bytes -> logger.finest("database-initializer::initialize::{}::content-size::{}",Area.class.getSimpleName(),bytes.length))
