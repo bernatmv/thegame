8ccb5f5aa0c8cd480e205b031047813402dd7f19
Bernat <bernatmv@gmail.com>
2017-05-25 11:47:27 +0200

    feat: added translations for the signup section

--- client/src/common/constants/translationConstants.ts
+++ client/src/common/constants/translationConstants.ts
@@ -1,123 +1,61 @@
 export const keyPrefix = 'thegame.';
 
 export default class TranslationConstants {
-    static get loginInputPlaceholder() {
-        return `${keyPrefix}loginInputPlaceholder`;
-    }
-
-    static get loginDivider() {
-        return `${keyPrefix}loginDivider`;
-    }
-
-    static get signUpButton() {
-        return `${keyPrefix}signUpButton`;
-    }
-
-    static get saySomething() {
-        return `${keyPrefix}saySomething`;
-    }
-
-    static get online() {
-        return `${keyPrefix}online`;
-    }
-
-    static get connecting() {
-        return `${keyPrefix}connecting`;
-    }
-
-    static get offline() {
-        return `${keyPrefix}offline`;
-    }
-
-    static get aMomentAgo() {
-        return `${keyPrefix}aMomentAgo`;
-    }
-
-    static get secondsAgo() {
-        return `${keyPrefix}secondsAgo`;
-    }
-
-    static get minute() {
-        return `${keyPrefix}minute`;
-    }
-
-    static get hour() {
-        return `${keyPrefix}hour`;
-    }
-
-    static get ago() {
-        return `${keyPrefix}ago`;
-    }
-
-    static get server() {
-        return `${keyPrefix}server`;
-    }
-
-    static get north() {
-        return `${keyPrefix}north`;
-    }
-
-    static get east() {
-        return `${keyPrefix}east`;
-    }
-
-    static get south() {
-        return `${keyPrefix}south`;
-    }
-
-    static get west() {
-        return `${keyPrefix}west`;
-    }
-
-    static get up() {
-        return `${keyPrefix}up`;
-    }
-
-    static get down() {
-        return `${keyPrefix}down`;
-    }
-
-    static get unvisitedLocation() {
-        return `${keyPrefix}unvisitedLocation`;
-    }
-
-    static get unvisitedDirection() {
-        return `${keyPrefix}unvisitedDirection`;
-    }
-
-    static get welcomeToTheGame() {
-        return `${keyPrefix}welcomeToTheGame`;
-    }
-
-    static get noObjectsCloseBy() {
-        return `${keyPrefix}noObjectsCloseBy`;
-    }
-
-    static get noEnemiesCloseBy() {
-        return `${keyPrefix}noEnemiesCloseBy`;
-    }
-
-    static get thereIs() {
-        return `${keyPrefix}thereIs`;
-    }
-
-    static get youSee() {
-        return `${keyPrefix}youSee`;
-    }
-
-    static get isHere() {
-        return `${keyPrefix}isHere`;
-    }
-
-    static get introLine1() {
-        return `${keyPrefix}introLine1`;
-    }
-
-    static get introLine2() {
-        return `${keyPrefix}introLine2`;
-    }
-
-    static get introLine3() {
-        return `${keyPrefix}introLine3`;
-    }
+    /* LOGIN */
+    static get loginInputPlaceholder() {return `${keyPrefix}loginInputPlaceholder`;}
+    static get loginDivider() {return `${keyPrefix}loginDivider`;}
+    static get signUpButton() {return `${keyPrefix}signUpButton`;}
+    /* SIGNUP */
+    static get signUpTitle() {return `${keyPrefix}signUpTitle`;}
+    static get signUpRaceLabel() {return `${keyPrefix}signUpRaceLabel`;}
+    static get signUpNameLabel() {return `${keyPrefix}signUpNameLabel`;}
+    static get signUpNamePlaceholder() {return `${keyPrefix}signUpNamePlaceholder`;}
+    static get signUpProfessionTitle() {return `${keyPrefix}signUpProfessionTitle`;}
+    static get signUpGenderTitle() {return `${keyPrefix}signUpGenderTitle`;}
+    static get signUpFinishTitle() {return `${keyPrefix}signUpFinishTitle`;}
+    /* GENDER */
+    static get genderMale() {return `${keyPrefix}genderMale`;}
+    static get genderFemale() {return `${keyPrefix}genderMale`;}
+    static get genderIntergender() {return `${keyPrefix}genderIntergender`;}
+    static get genderNeuter() {return `${keyPrefix}genderNeuter`;}
+    static get genderGenderless() {return `${keyPrefix}genderGenderless`;}
+    static get genderOther() {return `${keyPrefix}genderOther`;}
+    /* RACE */
+    static get raceHuman() {return `${keyPrefix}raceHuman`;}
+    static get raceGoblin() {return `${keyPrefix}raceGoblin`;}
+    /* PROFESSION */
+    static get professionWarrior() {return `${keyPrefix}professionWarrior`;}
+    static get professionWizard() {return `${keyPrefix}professionWizard`;}
+    /* CONNECTION */
+    static get server() {return `${keyPrefix}server`;}
+    static get online() {return `${keyPrefix}online`;}
+    static get connecting() {return `${keyPrefix}connecting`;}
+    static get offline() {return `${keyPrefix}offline`;}
+    /* CHAT */
+    static get saySomething() {return `${keyPrefix}saySomething`;}
+    static get welcomeToTheGame() {return `${keyPrefix}welcomeToTheGame`;}
+    static get aMomentAgo() {return `${keyPrefix}aMomentAgo`;}
+    static get secondsAgo() {return `${keyPrefix}secondsAgo`;}
+    static get minute() {return `${keyPrefix}minute`;}
+    static get hour() {return `${keyPrefix}hour`;}
+    static get ago() {return `${keyPrefix}ago`;}
+    /* DIRECTION */
+    static get north() {return `${keyPrefix}north`;}
+    static get east() {return `${keyPrefix}east`;}
+    static get south() {return `${keyPrefix}south`;}
+    static get west() {return `${keyPrefix}west`;}
+    static get up() {return `${keyPrefix}up`;}
+    static get down() {return `${keyPrefix}down`;}
+    static get unvisitedLocation() {return `${keyPrefix}unvisitedLocation`;}
+    static get unvisitedDirection() {return `${keyPrefix}unvisitedDirection`;}
+    /* ROOM FOOTER */
+    static get noObjectsCloseBy() {return `${keyPrefix}noObjectsCloseBy`;}
+    static get noEnemiesCloseBy() {return `${keyPrefix}noEnemiesCloseBy`;}
+    static get thereIs() {return `${keyPrefix}thereIs`;}
+    static get youSee() {return `${keyPrefix}youSee`;}
+    static get isHere() {return `${keyPrefix}isHere`;}
+    /* INTRO */
+    static get introLine1() {return `${keyPrefix}introLine1`;}
+    static get introLine2() {return `${keyPrefix}introLine2`;}
+    static get introLine3() {return `${keyPrefix}introLine3`;}
 }
