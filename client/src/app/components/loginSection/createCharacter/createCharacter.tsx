20296b460b4748d5ec938d3260602349a631d141
Bernat <bernatmv@gmail.com>
2017-05-26 11:01:49 +0200

    feat: added game constants and fixed state management in the signup form

--- client/src/app/components/loginSection/createCharacter/createCharacter.tsx
+++ client/src/app/components/loginSection/createCharacter/createCharacter.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import CreateCharacterProps from './createCharacterProps';
 import {Dropdown, Segment, Input, Icon, Divider, Button, Header, Image} from 'semantic-ui-react';
-import PlayerDto from '../../../../common/service/dtos/playerDto';
+import GameConstants from '../../../../common/constants/gameConstants';
 import TranslationConstants from '../../../../common/constants/translationConstants';
 import i18nService from '../../../../common/service/i18nServiceImpl';
 import * as styles from './createCharacter.css';
@@ -16,18 +16,34 @@ export default class Login extends React.Component<CreateCharacterProps, CreateC
     constructor(props?: CreateCharacterProps, context?: any) {
         super(props, context);
         this.state = {
-            model: null
+            race: GameConstants.Human,
+            name: '',
+            validName: false,
+            profession: GameConstants.Warrior,
+            gender: null
         };
     }
 
+    private _onChangeRace = (event, data) => this.setState({race: data.value});
+    private _onChangeName = (event, data) => {
+        this.setState({name: data.value});
+        // TODO: checlk with server name availability
+        if (!!data.value) {
+            this.setState({validName: true});
+        }
+    };
+    private _onChangeProfession = (event, data) => this.setState({profession: data.value});
+    private _onChangeGender = (event, data) => this.setState({gender: data.value});
+
     render(): JSX.Element {
+        let raceImage = (this.state.race === GameConstants.Goblin) ? goblinImage : humanImage;
         return <div className={styles.signup}>
             
             <Divider horizontal inverted section style={{fontSize: 11}}>
                 {i18nService.Instance.translate(TranslationConstants.signUpTitle)}
             </Divider>
             <Header as='h4' inverted>
-                <Image src={humanImage} size='mini' shape='circular' />
+                <Image src={raceImage} size='mini' shape='circular' />
                 {' '}
                 <Header.Content>
                     {i18nService.Instance.translate(TranslationConstants.signUpRaceLabel)}
@@ -36,6 +52,7 @@ export default class Login extends React.Component<CreateCharacterProps, CreateC
                         inline 
                         options={races} 
                         defaultValue={races[0].value} 
+                        onChange={this._onChangeRace}
                         className={styles.signup__dropdown} />
                 </Header.Content>
             </Header>
@@ -46,8 +63,9 @@ export default class Login extends React.Component<CreateCharacterProps, CreateC
             <Input
                 icon={<Icon name='pencil' inverted />}
                 iconPosition='left'
-                label={{icon: 'check', color: 'green'}}
-                labelPosition='right corner'
+                label={this.state.validName ? {icon: 'check', color: 'green'} : null}
+                labelPosition={this.state.validName ? 'right corner' : null}
+                onChange={this._onChangeName}
                 placeholder={i18nService.Instance.translate(TranslationConstants.signUpNamePlaceholder)} />
 
             <Divider horizontal inverted section style={{fontSize: 11}}>
@@ -56,7 +74,7 @@ export default class Login extends React.Component<CreateCharacterProps, CreateC
             <Dropdown 
                 options={professions} 
                 defaultValue={professions[0].value} 
-                style={inlineDropdownStyle} 
+                onChange={this._onChangeProfession}
                 className={styles.signup__dropdown} />
 
             <Divider horizontal inverted section style={{fontSize: 11}}>
@@ -64,15 +82,45 @@ export default class Login extends React.Component<CreateCharacterProps, CreateC
             </Divider>
             <div>
                 <Button.Group labeled inverted vertical color='green'>
-                    <Button icon='man'          content={i18nService.Instance.translate(TranslationConstants.genderMale)} />
-                    <Button icon='woman'        content={i18nService.Instance.translate(TranslationConstants.genderFemale)} />
-                    <Button icon='intergender'  content={i18nService.Instance.translate(TranslationConstants.genderIntergender)} />
+                    <Button 
+                        active={this.state.gender === GameConstants.Male}
+                        value={GameConstants.Male} 
+                        icon='man' 
+                        content={i18nService.Instance.translate(TranslationConstants.genderMale)} 
+                        onClick={this._onChangeGender}/>
+                    <Button 
+                        active={this.state.gender === GameConstants.Female}
+                        value={GameConstants.Female} 
+                        icon='woman' 
+                        content={i18nService.Instance.translate(TranslationConstants.genderFemale)} 
+                        onClick={this._onChangeGender}/>
+                    <Button 
+                        active={this.state.gender === GameConstants.Intergender}
+                        value={GameConstants.Intergender} 
+                        icon='intergender' 
+                        content={i18nService.Instance.translate(TranslationConstants.genderIntergender)} 
+                        onClick={this._onChangeGender}/>
                 </Button.Group>
                 {' '}
                 <Button.Group labeled inverted vertical color='green'>
-                    <Button icon='neuter'       content={i18nService.Instance.translate(TranslationConstants.genderNeuter)} />
-                    <Button icon='genderless'   content={i18nService.Instance.translate(TranslationConstants.genderGenderless)} />
-                    <Button icon='other gender' content={i18nService.Instance.translate(TranslationConstants.genderOther)} />
+                    <Button 
+                        active={this.state.gender === GameConstants.Neuter}
+                        value={GameConstants.Neuter}
+                        icon='neuter'
+                        content={i18nService.Instance.translate(TranslationConstants.genderNeuter)} 
+                        onClick={this._onChangeGender}/>
+                    <Button 
+                        active={this.state.gender === GameConstants.Genderless}
+                        value={GameConstants.Genderless}
+                        icon='genderless'
+                        content={i18nService.Instance.translate(TranslationConstants.genderGenderless)} 
+                        onClick={this._onChangeGender}/>
+                    <Button 
+                        active={this.state.gender === GameConstants.OtherGender}
+                        value={GameConstants.OtherGender}
+                        icon='other gender'
+                        content={i18nService.Instance.translate(TranslationConstants.genderOther)} 
+                        onClick={this._onChangeGender}/>
                 </Button.Group>
             </div>
 
@@ -83,7 +131,7 @@ export default class Login extends React.Component<CreateCharacterProps, CreateC
                 inverted 
                 circular 
                 icon='heart' 
-                size='massive' 
+                size='huge' 
                 color='green' 
                 className={animations.beat} />
         </div>;
@@ -91,17 +139,19 @@ export default class Login extends React.Component<CreateCharacterProps, CreateC
 }
 
 interface CreateCharacterState {
-    model: PlayerDto;
+    race: string;
+    name: string;
+    validName: boolean;
+    profession: string;
+    gender: string;
 }
-
-const inlineDropdownStyle = {borderBottom: 'dashed 1px white'};
 
 // TODO: we have to get race and profession from each dto in the assets once we have it defined
 const races = [
     {
-        key: 'human',
+        key: GameConstants.Human,
         text: i18nService.Instance.translate(TranslationConstants.raceHuman).toLocaleLowerCase(),
-        value: 'human',
+        value: GameConstants.Human,
         content: <Segment inverted className={styles.signup__dropdown__segment}>
                     <Header inverted size='small' color='grey'>
                         <Image src={humanImage} size='mini' shape='circular' />
@@ -110,9 +160,9 @@ const races = [
                 </Segment>
     },
     {
-        key: 'goblin',
+        key: GameConstants.Goblin,
         text: i18nService.Instance.translate(TranslationConstants.raceGoblin).toLocaleLowerCase(),
-        value: 'goblin',
+        value: GameConstants.Goblin,
         content: <Segment inverted className={styles.signup__dropdown__segment}>
                     <Header inverted size='small' color='grey'>
                         <Image src={goblinImage} size='mini' shape='circular' />
@@ -125,9 +175,9 @@ const races = [
 // TODO: we have to get race and profession from each dto in the assets once we have it defined
 const professions = [
     {
-        key: 'warrior',
+        key: GameConstants.Warrior,
         text: i18nService.Instance.translate(TranslationConstants.professionWarrior).toLocaleLowerCase(),
-        value: 'warrior',
+        value: GameConstants.Warrior,
         content: <Segment inverted className={styles.signup__dropdown__segment}>
                     <Header inverted size='small' color='grey'>
                         <Image src={warriorImage} size='mini' shape='circular' />
@@ -136,9 +186,9 @@ const professions = [
                 </Segment>
     },
     {
-        key: 'wizard',
+        key: GameConstants.Wizard,
         text: i18nService.Instance.translate(TranslationConstants.professionWizard).toLocaleLowerCase(),
-        value: 'wizard',
+        value: GameConstants.Wizard,
         content: <Segment inverted className={styles.signup__dropdown__segment}>
                     <Header inverted size='small' color='grey'>
                         <Image src={wizardImage} size='mini' shape='circular' />
