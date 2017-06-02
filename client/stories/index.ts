585c6116ef01859187e78700b3025120cdc94713
Bernat <bernatmv@gmail.com>
2017-06-02 12:59:31 +0200

    feat: fixed storybook workbench styles

--- client/stories/index.ts
+++ client/stories/index.ts
@@ -1,1 +1,22 @@
+import './storybook.css';
+
 import './direction';
+
+//->General styles in storybook to look like the game
+
+//*INPUT
+//*BUTTON
+//BAR
+//CHATITEM
+//CONNECTIONSTATUS
+//PROFILE
+//ROOMSECTION
+//->Direction
+//->Footer
+//CHATSECTION
+//AVATARSECTION
+//ENEMYSECTION
+//INTROSECTION
+//LOGINSECTION
+//->CREATECHARACTER
+//SYSTEMSECTION
